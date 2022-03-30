b1fcc6af35d47adfd2e6b6a30d7a094c8b46be91
thomas <thomasdelmoro@gmail.com>
2022-03-30 19:59:35 +0200

    Aggiunta euristica Linear Conflicts

--- Heuristics.py
+++ Heuristics.py
@@ -7,34 +7,55 @@ import numpy as np
 from queue import Queue
 import Puzzle
 
-def heuristic(node, h):
-    #Manhattan distance
-    if h == 1:
-        d = 0
-        for i in range(4):
-            for j in range(4):
-                if node[i, j] != 0:
-                    x = node[i, j] % 4
-                    y = math.floor(node[i, j] / 4)
-                    d = d + abs(i-y) + abs(j-x)
-        return d
 
-    #Non-additive pattern databases
-    #if h == 2:
-        
+def heuristic(node, h):
+    if h == 1:
+        result = manhattanDistance(node)
+
+    elif h == 2:
+        result = linearConflicts(node)
+
+    return result
+
+
+def manhattanDistance(node):
+    d = 0
+    for i in range(4):
+        for j in range(4):
+            if node[i, j] != 0:
+                x = node[i, j] % 4
+                y = math.floor(node[i, j] / 4)
+                d = d + abs(i - y) + abs(j - x)
+    return d
+
+
+def linearConflicts(node):
+    d = 0
+    for i in range(4):
+        for j in range(4):
+            if node[i, j] != 0:
+                x = node[i, j] % 4
+                y = math.floor(node[i, j] / 4)
+                d += abs(i - y) + abs(j - x)
+                if x == 0 & y != 0:
+                    d += 2
+                if x != 0 & y == 0:
+                    d += 2
+    return d
+
 
 def nonAdditive():
-    start = np.array([0,1,1,3],[1,1,1,7],[1,1,1,11],[12,13,14,15])
+    start = np.array([0, 1, 1, 3], [1, 1, 1, 7], [1, 1, 1, 11], [12, 13, 14, 15])
     node = Puzzle.Node(start, 0)
-    frontier = Queue()  #FIFO queue
+    frontier = Queue()  # FIFO queue
     frontier.put(node)
-    reached = []
-    reached.append(tuple([tuple(e) for e in node.state]))
+    reached = dict()
+    reached[tuple([tuple(e) for e in node.state])] = node
     while frontier.qsize() > 0:
         node = frontier.get()
         neighbors = Puzzle.expand(node)
         for child in neighbors:
             s = tuple([tuple(e) for e in child.state])
             if s not in reached:
-                reached.append(s)
+                reached[s] = child
                 frontier.put(child)
--- main.py
+++ main.py
@@ -8,10 +8,10 @@ import numpy as np
 import Puzzle
 from queue import PriorityQueue
 
-N = 1
+N = 2
 
 def main():
-    start = np.array([[1,3,7,0],[4,5,2,11],[13,9,6,15],[8,12,14,10]])
+    start = np.array([[1,3,7,4],[0,5,2,11],[13,9,6,15],[8,12,14,10]])
     goal = np.array([[0,1,2,3],[4,5,6,7],[8,9,10,11],[12,13,14,15]])
 
     print(start)
