6c2453dc783895c037fe87c8bbf5b6354027c50f
thomas <thomasdelmoro@gmail.com>
2022-03-30 16:35:29 +0200

    Aggiunto metodo nonAdditive(BFS) da finire

--- Heuristics.py
+++ Heuristics.py
@@ -3,7 +3,9 @@
 '''
 
 import math
-
+import numpy as np
+from queue import Queue
+import Puzzle
 
 def heuristic(node, h):
     #Manhattan distance
@@ -20,3 +22,19 @@ def heuristic(node, h):
     #Non-additive pattern databases
     #if h == 2:
         
+
+def nonAdditive():
+    start = np.array([0,1,1,3],[1,1,1,7],[1,1,1,11],[12,13,14,15])
+    node = Puzzle.Node(start, 0)
+    frontier = Queue()  #FIFO queue
+    frontier.put(node)
+    reached = []
+    reached.append(tuple([tuple(e) for e in node.state]))
+    while frontier.qsize() > 0:
+        node = frontier.get()
+        neighbors = Puzzle.expand(node)
+        for child in neighbors:
+            s = tuple([tuple(e) for e in child.state])
+            if s not in reached:
+                reached.append(s)
+                frontier.put(child)
--- Puzzle.py
+++ Puzzle.py
@@ -16,40 +16,39 @@ class Node:
     def __lt__(self, other):
         return True
 
+
+def expand(node):
+    neighbors = []
+    i = np.where(node.state == 0)[0][0]
+    j = np.where(node.state == 0)[1][0]
+
+    if i != 0:
+        newState = node.state.copy()
+        newState[i - 1, j], newState[i, j] = newState[i, j], newState[i - 1, j]
+        neighbors.append(Node(newState, node.pathCost + 1))
+
+    if i != 3:
+        newState = node.state.copy()
+        newState[i, j], newState[i + 1, j] = newState[i + 1, j], newState[i, j]
+        neighbors.append(Node(newState, node.pathCost + 1))
+
+    if j != 0:
+        newState = node.state.copy()
+        newState[i, j - 1], newState[i, j] = newState[i, j], newState[i, j - 1]
+        neighbors.append(Node(newState, node.pathCost + 1))
+
+    if j != 3:
+        newState = node.state.copy()
+        newState[i, j], newState[i, j + 1] = newState[i, j + 1], newState[i, j]
+        neighbors.append(Node(newState, node.pathCost + 1))
+    return neighbors
+
+
 class Puzzle:
     def __init__(self, start, goal):
         self.start = start
         self.goal = goal
         self.currentNode = None
-
-    def stateRepr(self, state):
-        return str(state)
-
-    def expand(self, node):
-        neighbors = []
-        i = np.where(node.state == 0)[0][0]
-        j = np.where(node.state == 0)[1][0]
-
-        if i != 0:
-            newState = node.state.copy()
-            newState[i - 1, j], newState[i, j] = newState[i, j], newState[i - 1, j]
-            neighbors.append(Node(newState, node.pathCost + 1))
-
-        if i != 3:
-            newState = node.state.copy()
-            newState[i, j], newState[i + 1, j] = newState[i + 1, j], newState[i, j]
-            neighbors.append(Node(newState, node.pathCost + 1))
-
-        if j != 0:
-            newState = node.state.copy()
-            newState[i, j - 1], newState[i, j] = newState[i, j], newState[i, j - 1]
-            neighbors.append(Node(newState, node.pathCost + 1))
-
-        if j != 3:
-            newState = node.state.copy()
-            newState[i, j], newState[i, j + 1] = newState[i, j + 1], newState[i, j]
-            neighbors.append(Node(newState, node.pathCost + 1))
-        return neighbors
 
     def evaluationFunction(self, node, h):
         f = node.pathCost + heuristic(node.state, h)
@@ -67,7 +66,7 @@ class Puzzle:
             if np.array_equiv(node.state, self.goal):
                 return node
             print(len(reached))
-            neighbors = self.expand(node)
+            neighbors = expand(node)
             for child in neighbors:
                 s = tuple([tuple(e) for e in child.state])
                 if s not in reached.keys() or child.pathCost < reached[s].pathCost:
