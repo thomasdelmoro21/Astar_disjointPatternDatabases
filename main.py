cc79252fc40b198e20de0bf85d100f982af9d663
thomas <thomasdelmoro@gmail.com>
2022-04-04 19:42:26 +0200

    Modificati test

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from queue import PriorityQueue
 N = 2
 
 def main():
-    start = np.array([[1,3,7,4],[0,5,2,11],[13,9,6,15],[8,12,14,10]])
+    start = np.array([[1,2,3,7], [4,5,6,10], [0,8,11,15],[12,9,13,14]])
     goal = np.array([[0,1,2,3],[4,5,6,7],[8,9,10,11],[12,13,14,15]])
 
     print(start)
