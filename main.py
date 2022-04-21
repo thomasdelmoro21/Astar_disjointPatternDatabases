e0d4ca6397171d3ff6dfee1edd51fdc1c5c324f7
thomas <thomasdelmoro@gmail.com>
2022-04-21 20:03:36 +0200

    Fatto merge in master del branch Liste e sistemati i test

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ import numpy as np
 import Puzzle
 from queue import PriorityQueue
 
-N = 2
+N = 1
 
 def main():
-    start = np.array([[1,3,7,4],[0,5,2,11],[13,9,6,15],[8,12,14,10]])
-    goal = np.array([[0,1,2,3],[4,5,6,7],[8,9,10,11],[12,13,14,15]])
+    start = [1,2,3,7, 0,5,10,6, 4,8,11,15, 12,9,13,14]
+    goal = [0,1,2,3, 4,5,6,7, 8,9,10,11, 12,13,14,15]
 
     print(start)
     puzzle = Puzzle.Puzzle(start, goal)
